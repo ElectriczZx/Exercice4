b9b45aa5fffd2fc4fc749b70f90ca65adf36ebe2
Nico Isbarn <n.isbarn17@gmail.com>
2020-10-24 16:28:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Squareroot sqrt = new Squareroot();
+
+            Console.Write("Please input a Number: ");
+            sqrt.setNumber(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Squareroot of: "+sqrt.getNumber()+" is: "+sqrt.getSqrt());
+        }
+    }
+}
---
+++ Squareroot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+
+namespace Exercice4
+{
+    class Squareroot
+    {
+
+        double num;
+        public void setNumber(double nb)
+        {
+            num = nb;
+        }
+        
+        public double getSqrt()
+        {
+            return Math.Sqrt(num);
+        }
+        public double getNumber()
+        {
+            return num;
+        }
+
+    }
+}
